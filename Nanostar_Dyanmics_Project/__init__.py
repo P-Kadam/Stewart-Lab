b21d8687c61f26c685871d4ba1b82970e1987578
P-Kadam <kadam.pr22@gmail.com>
2025-09-14 22:20:31 -0700

    top-level API
    Added a top-level API script & moved merge_data out of Visualization&Analysis folder

---
+++ Nanostar_Dyanmics_Project/__init__.py
@@ -0,0 +1,15 @@
+# myproject/__init__.py
+
+#DATA
+from .Read_Tracjectory import prep_trajectory_data
+from .Calc_Angles import calc_angles
+from .MergeData import merge_data
+
+#VISUALS
+from .Visualization&Analysis.Interpolated_Histogram import plot_interpolated_histogram
+from .Visualization&Analysis.Violin_Plot import plot_violin
+
+
+__all__ = ["prep_trajectory_data", "calc_angles", "merge_data", "plot_interpolated_histogram",
+           "plot_violin"]
+
